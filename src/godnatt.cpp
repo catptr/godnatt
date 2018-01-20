5905863fe68c19413a3f990870a14dd5c71c5fb2
Viktor Johansson <viktor.krabba@gmail.com>
2018-01-20 15:35:54 +0100

    Adds ReadEntireFile which returns contents of text file

--- src/godnatt.cpp
+++ src/godnatt.cpp
@@ -1,22 +1,46 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-
-#ifdef DEBUG
+#include <stdlib.h>
 #include <stdio.h>
-#endif
 
 #define UNUSED(x) (void)(x)
+
+char *ReadEntireFile(const char *Path)
+{
+    FILE *File = nullptr;
+    errno_t Error = fopen_s(&File, Path, "rb");
+    if (Error != 0)
+    {
+        return nullptr;
+    }
+
+    fseek(File, 0, SEEK_END);
+    size_t Size = (size_t)ftell(File);
+    fseek(File, 0, SEEK_SET);
+
+    char *Contents = (char *)malloc(sizeof(char) * (Size + 1));
+    size_t NumRead = fread(Contents, 1, Size, File);
+    fclose(File);
+
+    if (NumRead < Size)
+    {
+        free(Contents);
+        return nullptr;
+    }
+
+    Contents[Size] = '\0';
+
+    return Contents;
+}
 
 int main(int argc, char *argv[])
 {
     UNUSED(argc);
     UNUSED(argv);
-    
-#ifdef DEBUG
-    printf("Hello, world!\n");
-#else
-    MessageBoxA(0, "Hello, world!", "godnatt", MB_OK | MB_ICONINFORMATION);
-#endif
+
+    char *Bedtimes = ReadEntireFile("bedtimes.txt");
+    printf("%s", Bedtimes);
+    free(Bedtimes);
     
     return 0;
 }
