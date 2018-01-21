81bc163fd8c36c223cfa5c7fe91aaa80475c8176
Viktor Johansson <viktor.krabba@gmail.com>
2018-01-21 13:41:14 +0100

    Adds --lock flag to be used by the task scheduler

--- src/godnatt.cpp
+++ src/godnatt.cpp
@@ -6,9 +6,21 @@
 
 int main(int argc, char *argv[])
 {
-    Unused(argc);
-    Unused(argv);
-
-    const char *Path = "G:\\godnatt\\bedtimes.txt";
-    return WatchFile(Path);
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "--lock") == 0)
+        {
+            if (!LockWorkStation())
+            {
+                ShowError("[ERROR::Lock Workstation] Failed with error code: %x\n", GetLastError());
+                return 2;
+            }
+        }
+    }
+    else
+    {
+        // Default to watch bedtimes file for changes
+        const char *Path = "G:\\godnatt\\bedtimes.txt";
+        return WatchFile(Path);
+    }
 }
