6d1187b7001c467d693240da45e44ceb09bc5bd6
Viktor Johansson <viktor.krabba@gmail.com>
2018-01-21 13:53:12 +0100

    Removes compiler warnings caused by com headers

--- build.bat
+++ build.bat
@@ -40,7 +40,7 @@ if %RELEASE_MODE% equ 0 ( rem debug
     echo Building release...
 )
 
-set WARNINGS= /Wall /WX /wd4514 /wd4710 /wd4711 /wd4820
+set WARNINGS= /Wall /WX /wd4514 /wd4710 /wd4711 /wd4820 /wd4365
 
 if %RELEASE_MODE% equ 0 ( rem debug
     :: Unreferenced formal parameter and local variables
